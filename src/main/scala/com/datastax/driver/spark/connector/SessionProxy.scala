af71db35581dc3e3dd9fac1ea72a315f7d8c3519
Piotr Kolaczkowski <pkolaczk@datastax.com>
2014-07-05 19:50:05 +0200

    Caching of PreparedStatements. Subsequent preparations of the same PreparedStatement are returned from the cache and don't cause a warning. Fixes #3.

--- src/main/scala/com/datastax/driver/spark/connector/SessionProxy.scala
+++ src/main/scala/com/datastax/driver/spark/connector/SessionProxy.scala
@@ -1,16 +1,28 @@
 package com.datastax.driver.spark.connector
 
 import java.lang.reflect.{Proxy, Method, InvocationHandler}
-import com.datastax.driver.core.Session
+import com.datastax.driver.core.{RegularStatement, SimpleStatement, Session}
+import org.apache.avro.generic.GenericData.StringType
 
-/** Wraps a `Session` and intercepts `close` method to invoke `afterClose` handler. */
+/** Wraps a `Session` and intercepts:
+  *  - `close` method to invoke `afterClose` handler
+  *  - `prepare` methods to cache `PreparedStatement` objects. */
 class SessionProxy(session: Session, afterClose: Session => Any) extends InvocationHandler {
 
   private var closed = false
 
   override def invoke(proxy: Any, method: Method, args: Array[AnyRef]) = {
     try {
-      method.invoke(session, args: _*)
+      val StringClass = classOf[String]
+      val RegularStatementClass = classOf[String]
+
+      (method.getName, method.getParameterTypes) match {
+        case ("prepare", Array(StringClass)) =>
+          PreparedStatementCache.prepareStatement(session, new SimpleStatement(args(0).asInstanceOf[String]))
+        case ("prepare", Array(RegularStatementClass)) =>
+          PreparedStatementCache.prepareStatement(session, args(0).asInstanceOf[RegularStatement])
+        case _ => method.invoke(session, args: _*)
+      }
     }
     finally {
       if (method.getName == "close" && !closed) {
@@ -23,9 +35,15 @@ class SessionProxy(session: Session, afterClose: Session => Any) extends Invocat
 
 object SessionProxy {
 
-  /** Registers a callback on `Session#close` method.
+  /** Creates a new `SessionProxy` delegating to the given `Session`.
+    * The proxy adds prepared statement caching functionality. */
+  def wrap(session: Session): Session =
+    wrapWithCloseAction(session)(_ => ())
+
+  /** Creates a new `SessionProxy` delegating to the given `Session`.
+    * Additionally registers a callback on `Session#close` method.
     * @param afterClose code to be invoked after the session has been closed */
-  def withCloseAction(session: Session)(afterClose: Session => Any): Session =
+  def wrapWithCloseAction(session: Session)(afterClose: Session => Any): Session =
     Proxy.newProxyInstance(
       session.getClass.getClassLoader,
       Array(classOf[Session]),
