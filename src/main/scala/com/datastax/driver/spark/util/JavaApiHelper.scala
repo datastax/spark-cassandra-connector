190ca7ea54088ec3484a39a5e9972cff375e4e1d
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2014-07-10 17:16:32 +0200

    Removed unused methods from JavaApiHelper, fixes #11

--- src/main/scala/com/datastax/driver/spark/util/JavaApiHelper.scala
+++ src/main/scala/com/datastax/driver/spark/util/JavaApiHelper.scala
@@ -1,30 +1,19 @@
 package com.datastax.driver.spark.util
 
-
-import com.datastax.driver.spark.{RDDFunctions, SparkContextFunctions}
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
-
-import scala.reflect.ClassTag
 import scala.reflect.api._
 import scala.reflect.runtime.universe._
 import scala.reflect.api.{Mirror, TypeCreator}
 
-/**
- * Some helper 
- */
+/** A helper class to make it possible to access components written in Scala from Java code. */
 object JavaApiHelper {
 
+  /** Returns a [[TypeTag]] for the given class. */
   def getTypeTag[T](clazz: Class[T]): Universe#TypeTag[T] = {
     TypeTag.apply(runtimeMirror(Thread.currentThread().getContextClassLoader), new TypeCreator {
-      override def apply[U <: Universe with Singleton](m: Mirror[U]): U # Type ={
+      override def apply[U <: Universe with Singleton](m: Mirror[U]): U#Type = {
         m.staticClass(clazz.getName).toTypeConstructor
       }
     })
   }
-  
-  def getSparkContextFunctions(sc: SparkContext): SparkContextFunctions = new SparkContextFunctions(sc)
-  
-  def getRDDFunctions[T: ClassTag](rdd: RDD[T]): RDDFunctions[T] = new RDDFunctions[T](rdd)
-  
+
 }
