5487922dab60d75097f46636d8f827a157b548eb
Helena Edelson <helena.edelson@datastax.com>
2014-07-28 18:13:31 -0400

    101 clarifying the usage of <= in a test assertion

--- src/it/scala/com/datastax/spark/connector/streaming/ActorStreamSpec.scala
+++ src/it/scala/com/datastax/spark/connector/streaming/ActorStreamSpec.scala
@@ -39,6 +39,7 @@ class ActorStreamingSpec extends ActorSpec {
 
       expectMsgPF(duration) { case Terminated(ref) =>
         val rdd = ssc.cassandraTable[WordCount]("streaming_test", "words").select("word", "count")
+        // `<=` because we are in actor async land
         rdd.map(_.count).reduce(_ + _) should be <= (events * 2)
         rdd.toArray.size should be (data.size)
       }
