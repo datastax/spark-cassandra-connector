71e6a6b1518e6c5c93fb9f7d589672a5f1397e0c
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2014-07-28 08:26:38 +0200

    Updated JavaApiDemo so that it includes examples of how to use the newly added methods

--- src/test/java/com/datastax/spark/connector/demo/JavaApiDemo.java
+++ src/test/java/com/datastax/spark/connector/demo/JavaApiDemo.java
@@ -1,8 +1,6 @@
 package com.datastax.spark.connector.demo;
 
 import com.datastax.spark.connector.CassandraRow;
-import com.datastax.spark.connector.demo.DemoApp;
-import com.datastax.spark.connector.demo.DemoApp$;
 import com.google.common.base.Objects;
 import org.apache.hadoop.util.StringUtils;
 import org.apache.spark.api.java.JavaRDD;
@@ -16,7 +14,87 @@ import java.util.List;
 
 import static com.datastax.spark.connector.CassandraJavaUtil.javaFunctions;
 
+/**
+ * This Spark application demonstrates how to use Spark Cassandra Connector with Java.
+ *
+ * In order to run it, you will need to run Cassandra database, and create the following
+ * keyspace, table and secondary index:
+ *
+ * <pre>
+ * CREATE KEYSPACE test WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};
+ *
+ * CREATE TABLE test.people (
+ *      id          INT,
+ *      name        TEXT,
+ *      birth_date  TIMESTAMP,
+ *      PRIMARY KEY (id)
+ * );
+ *
+ * CREATE INDEX people_name_idx ON test.people(name);
+ * </pre>
+ */
 public class JavaApiDemo implements Serializable {
+
+    public JavaApiDemo() {
+        // just an initialisation of Spark Context
+        DemoApp demoApp = DemoApp$.MODULE$.apply();
+        JavaSparkContext sc = new JavaSparkContext(demoApp.sc());
+
+        // here we are going to save some data to Cassandra...
+        List<Person> people = Arrays.asList(
+                Person.newInstance(1, "John", new Date()),
+                Person.newInstance(2, "Anna", new Date()),
+                Person.newInstance(3, "Andrew", new Date())
+        );
+        JavaRDD<Person> rdd = sc.parallelize(people);
+        javaFunctions(rdd, Person.class).saveToCassandra("test", "people");
+
+        // use case: we want to read that data as an RDD of CassandraRows and convert them to strings...
+        JavaRDD<String> cassandraRowsRDD = javaFunctions(sc).cassandraTable("test", "people").toJavaRDD()
+                .map(new Function<CassandraRow, String>() {
+                    @Override
+                    public String call(CassandraRow cassandraRow) throws Exception {
+                        return cassandraRow.toString();
+                    }
+                });
+        System.out.println("Data as CassandraRows: \n" + StringUtils.join("\n", cassandraRowsRDD.toArray()));
+
+        // use case: we want to read that data as an RDD of Person beans and also convert them to strings...
+        JavaRDD<String> rdd2 = javaFunctions(sc).cassandraTable("test", "people", Person.class).toJavaRDD()
+                .map(new Function<Person, String>() {
+                    @Override
+                    public String call(Person person) throws Exception {
+                        return person.toString();
+                    }
+                });
+        System.out.println("Data as Person beans: \n" + StringUtils.join("\n", rdd2.toArray()));
+
+        // use case: we want to filter rows on the database side with use of the where clause
+        JavaRDD<String> rdd3 = javaFunctions(sc).cassandraTable("test", "people", Person.class).toJavaRDD()
+                .where("name=?", "Anna").map(new Function<Person, String>() {
+                    @Override
+                    public String call(Person person) throws Exception {
+                        return person.toString();
+                    }
+                });
+        System.out.println("Data filtered by the where clause (name='Anna'): \n" + StringUtils.join("\n", rdd3.toArray()));
+
+        // use case: we want to explicitly set a projection on the column set
+        JavaRDD<String> rdd4 = javaFunctions(sc).cassandraTable("test", "people").toJavaRDD()
+                .select("id").map(new Function<CassandraRow, String>() {
+                    @Override
+                    public String call(CassandraRow cassandraRow) throws Exception {
+                        return cassandraRow.toString();
+                    }
+                });
+        System.out.println("Data with only 'id' column fetched: \n" + StringUtils.join("\n", rdd4.toArray()));
+
+        sc.stop();
+    }
+
+    public static void main(String[] args) {
+        new JavaApiDemo();
+    }
 
     public static class Person implements Serializable {
         private Integer id;
@@ -63,46 +141,6 @@ public class JavaApiDemo implements Serializable {
                     .add("birthDate", birthDate)
                     .toString();
         }
-    }
-
-    public JavaApiDemo() {
-        DemoApp demoApp = DemoApp$.MODULE$.apply();
-        JavaSparkContext sc = new JavaSparkContext(demoApp.sc());
-
-        // Here we are going to save some data to Cassandra...
-        List<Person> people = Arrays.asList(
-                Person.newInstance(1, "John", new Date()),
-                Person.newInstance(2, "Anna", new Date()),
-                Person.newInstance(3, "Andrew", new Date())
-        );
-        JavaRDD<Person> rdd = sc.parallelize(people);
-        javaFunctions(rdd, Person.class).saveToCassandra("test", "people");
-
-        // then, we want to read that data as an RDD of CassandraRows and convert them to strings...
-        JavaRDD<String> cassandraRowsRDD = javaFunctions(sc).cassandraTable("test", "people").toJavaRDD()
-                .map(new Function<CassandraRow, String>() {
-                    @Override
-                    public String call(CassandraRow cassandraRow) throws Exception {
-                        return cassandraRow.toString();
-                    }
-                });
-        System.out.println("Data as CassandraRows: \n" + StringUtils.join("\n", cassandraRowsRDD.toArray()));
-
-        // finally, we want to read that data as an RDD of Person beans and also convert them to strings...
-        JavaRDD<String> rdd2 = javaFunctions(sc).cassandraTable("test", "people", Person.class).toJavaRDD()
-                .map(new Function<Person, String>() {
-                    @Override
-                    public String call(Person person) throws Exception {
-                        return person.toString();
-                    }
-                });
-        System.out.println("Data as Person beans: \n" + StringUtils.join("\n", rdd2.toArray()));
-
-        sc.stop();
-    }
-
-    public static void main(String[] args) {
-        new JavaApiDemo();
     }
 
 }
