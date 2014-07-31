8d29eba506351e77807823c05a073a41a1126051
Helena Edelson <helena.edelson@datastax.com>
2014-07-31 12:10:28 -0400

    #120 Added Writeable trait for DStream - RDD shared functionality

--- src/main/scala/com/datastax/spark/connector/RDDFunctions.scala
+++ src/main/scala/com/datastax/spark/connector/RDDFunctions.scala
@@ -9,7 +9,7 @@ import org.apache.spark.rdd.RDD
 import scala.reflect.ClassTag
 
 /** Provides Cassandra-specific methods on `RDD` */
-class RDDFunctions[T : ClassTag](rdd: RDD[T]) extends Serializable {
+class RDDFunctions[T : ClassTag](rdd: RDD[T]) extends WritableToCassandra[T] with Serializable {
 
   private lazy val batchSizeInRowsStr = rdd.sparkContext.getConf.get(
     "spark.cassandra.output.batch.size.rows", "auto")
@@ -70,23 +70,6 @@ class RDDFunctions[T : ClassTag](rdd: RDD[T]) extends Serializable {
     * By default, writes are performed at ConsistencyLevel.ONE in order to leverage data-locality and minimize network traffic.
     * This write consistency level is controlled by the following property:
     *   - spark.cassandra.output.consistency.level: consistency level for RDD writes, string matching the ConsistencyLevel enum name.
-    *
-    * @param keyspaceName The keyspace to use.
-    *
-    * @param tableName The table name to use
-    *
-    * @param columnNames The list of columns to save data to.
-    *                    If specified, uses only the unique column names, and you must select at least all primary key
-    *                    columns. All other fields are discarded. Non-selected property/column names are left unchanged.
-    *                    If not specified, will save data to all columns in the Cassandra table.
-    *                    Defaults to all columns: `Fields.ALL`.
-    *
-    * @param batchSize The batch size. By default, if the batch size is unspecified, the right amount
-    *                  is calculated automatically according the average row size. Specify explicit value
-    *                  here only if you find automatically tuned batch size doesn't result in optimal performance.
-    *                  Larger batches raise memory use by temporary buffers and may incur larger GC pressure on the server.
-    *                  Small batches would result in more roundtrips and worse throughput. Typically sending a few kilobytes
-    *                  of data per every batch is enough to achieve good performance.
     */
   def saveToCassandra(keyspaceName: String,
                       tableName: String,
@@ -117,4 +100,33 @@ class RDDFunctions[T : ClassTag](rdd: RDD[T]) extends Serializable {
       batchSizeInBytes = batchSizeInBytes,
       batchSizeInRows = batchSizeInRows,
       parallelismLevel = writeParallelismLevel)
+}
+
+trait WritableToCassandra[T] {
+
+  /**
+   * Saves the data from `RDD` to a Cassandra table.
+   *
+   * @param keyspaceName The keyspace to use.
+   *
+   * @param tableName The table name to use
+   *
+   * @param columnNames The list of columns to save data to.
+   *                    If specified, uses only the unique column names, and you must select at least all primary key
+   *                    columns. All other fields are discarded. Non-selected property/column names are left unchanged.
+   *                    If not specified, will save data to all columns in the Cassandra table.
+   *                    Defaults to all columns: `Fields.ALL`.
+   *
+   * @param batchSize The batch size. By default, if the batch size is unspecified, the right amount
+   *                  is calculated automatically according the average row size. Specify explicit value
+   *                  here only if you find automatically tuned batch size doesn't result in optimal performance.
+   *                  Larger batches raise memory use by temporary buffers and may incur larger GC pressure on the server.
+   *                  Small batches would result in more roundtrips and worse throughput. Typically sending a few kilobytes
+   *                  of data per every batch is enough to achieve good performance.
+   */
+  def saveToCassandra(keyspaceName: String,
+                      tableName: String,
+                      columnNames: Seq[String] = Fields.ALL,
+                      batchSize: Option[Int] = None)(implicit rwf: RowWriterFactory[T])
+
 }
--- src/main/scala/com/datastax/spark/connector/streaming/DStreamFunctions.scala
+++ src/main/scala/com/datastax/spark/connector/streaming/DStreamFunctions.scala
@@ -1,32 +1,14 @@
 package com.datastax.spark.connector.streaming
 
-import com.datastax.spark.connector._
-import com.datastax.spark.connector.writer.{Fields, TableWriter, RowWriterFactory}
 import org.apache.spark.streaming.dstream.DStream
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.writer.{Fields, RowWriterFactory}
 
 import scala.reflect.ClassTag
 
-class DStreamFunctions[T: ClassTag](dstream: DStream[T]) extends Serializable {
+class DStreamFunctions[T: ClassTag](dstream: DStream[T]) extends WritableToCassandra[T] with Serializable {
 
-  /** Performs [[com.datastax.spark.connector.RDDFunctions]] for each produced RDD.
-    *
-    * @param keyspaceName The keyspace to use
-    *
-    * @param tableName The table name to use
-    *
-    * @param columnNames The list of columns to save data to.
-    *                    If specified, uses only the unique column names, and you must select at least all primary key
-    *                    columns. All other fields are discarded. Non-selected property/column names are left unchanged.
-    *                    If not specified, will save data to all columns in the Cassandra table.
-    *                    Defaults to all columns: `TableWriter.AllColumns`.
-    *
-    * @param batchSize The batch size. By default, if the batch size is unspecified, the right amount
-    *                  is calculated automatically according the average row size. Specify explicit value
-    *                  here only if you find automatically tuned batch size doesn't result in optimal performance.
-    *                  Larger batches raise memory use by temporary buffers and may incur larger GC pressure on the server.
-    *                  Small batches would result in more roundtrips and worse throughput. Typically sending a few kilobytes
-    *                  of data per every batch is enough to achieve good performance. Defaults to `None`.
-    */
+  /** Performs [[com.datastax.spark.connector.WritableToCassandra.saveToCassandra()]] for each produced RDD. */
   def saveToCassandra(keyspaceName: String, tableName: String,
                       columnNames: Seq[String] = Fields.ALL,
                       batchSize: Option[Int] = None)(implicit rwf: RowWriterFactory[T]) {
