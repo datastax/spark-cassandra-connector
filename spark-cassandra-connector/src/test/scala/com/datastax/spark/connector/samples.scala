e0cec508348978a312b033f861962cbdfd3966a9
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2014-08-25 11:00:48 +0200

    Added test cases to detect bugs like #188

--- spark-cassandra-connector/src/test/scala/com/datastax/spark/connector/samples.scala
+++ spark-cassandra-connector/src/test/scala/com/datastax/spark/connector/samples.scala
@@ -2,25 +2,25 @@ package com.datastax.spark.connector {
 
 case class SampleScalaCaseClass(key: Int, value: String)
 
-class SampleScalaClass(val key: Int, val value: String)
+class SampleScalaClass(val key: Int, val value: String) extends Serializable
 
-class SampleScalaClassWithNoFields(key: Int, value: String)
+class SampleScalaClassWithNoFields(key: Int, value: String) extends Serializable
 
-class SampleScalaClassWithMultipleCtors(val key: Int, val value: String) {
+class SampleScalaClassWithMultipleCtors(var key: Int, var value: String) extends Serializable {
   def this(key: Int) = this(key, null)
 
   def this() = this(0, null)
 }
 
-class SampleWithNestedScalaCaseClass {
+class SampleWithNestedScalaCaseClass extends Serializable {
 
   case class InnerClass(key: Int, value: String)
 
 }
 
-class SampleWithDeeplyNestedScalaCaseClass {
+class SampleWithDeeplyNestedScalaCaseClass extends Serializable {
 
-  class IntermediateClass {
+  class IntermediateClass extends Serializable {
 
     case class InnerClass(key: Int, value: String)
 
