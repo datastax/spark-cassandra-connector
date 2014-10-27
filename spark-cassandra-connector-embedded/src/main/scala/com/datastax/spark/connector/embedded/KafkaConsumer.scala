cc41abc41cf7253f5b68e36d8ea27b88e4ce84b0
Helena Edelson <helena.edelson@datastax.com>
2014-10-27 13:12:52 -0400

    Improved KafkaConsumer for testing/prototyping.

--- spark-cassandra-connector-embedded/src/main/scala/com/datastax/spark/connector/embedded/KafkaConsumer.scala
+++ spark-cassandra-connector-embedded/src/main/scala/com/datastax/spark/connector/embedded/KafkaConsumer.scala
@@ -4,22 +4,19 @@ import java.util.concurrent.CountDownLatch
 import java.util.Properties
 import java.util.concurrent.Executors
 
-import scala.collection.JavaConversions._
-import kafka.consumer.ConsumerConfig
+import kafka.serializer.StringDecoder
+import kafka.consumer.{Consumer, ConsumerConfig}
 
 /** The KafkaConsumer is a very simple consumer of a single Kafka topic.
   * This is a helpful utility for IT tests to insure data is getting published to Kafka
   * for streaming ingestion upstream.
   */
-class KafkaConsumer(zookeeper: String, topic: String, groupId: String, numThreads: Int, latch: CountDownLatch) {
+class KafkaConsumer(zookeeper: String, topic: String, groupId: String, partitions: Int, numThreads: Int, latch: CountDownLatch) {
 
-  val  consumer = kafka.consumer.Consumer.createJavaConsumerConnector(createConsumerConfig)
-
-  // dude, that's dark:
-  val mapForStreams = mapAsJavaMap(Map(topic -> 1)).asInstanceOf[java.util.Map[java.lang.String, java.lang.Integer]]
+  val  consumer: kafka.consumer.ConsumerConnector = Consumer.create(createConsumerConfig)
 
   // create n partitions of the stream for topic “test”, to allow n threads to consume
-  val topicMessageStreams = consumer.createMessageStreams(mapForStreams)
+  val topicMessageStreams = consumer.createMessageStreams(Map(topic -> partitions), new StringDecoder(), new StringDecoder())
 
   val streams = topicMessageStreams.get(topic)
 
@@ -30,9 +27,11 @@ class KafkaConsumer(zookeeper: String, topic: String, groupId: String, numThread
   for(stream <- streams) {
     executor.submit(new Runnable() {
       def run() {
-        for(msgAndMetadata <- stream) {
-          println(s"Consumer received: ${new String(msgAndMetadata.message)}")
-          latch.countDown()
+        for(s <- stream) {
+          while(s.iterator.hasNext) {
+            println(s"Consumer (KafkaStream) received: ${new String(s.iterator.next.message)}")
+            latch.countDown()
+          }
         }
       }
     })
@@ -40,10 +39,11 @@ class KafkaConsumer(zookeeper: String, topic: String, groupId: String, numThread
 
   def createConsumerConfig: ConsumerConfig = {
     val props = new Properties()
+    props.put("consumer.timeout.ms", "2000")
     props.put("zookeeper.connect", zookeeper)
     props.put("group.id", groupId)
     props.put("zookeeper.session.timeout.ms", "400")
-    props.put("zookeeper.sync.time.ms", "200")
+    props.put("zookeeper.sync.time.ms", "10")
     props.put("auto.commit.interval.ms", "1000")
 
     new ConsumerConfig(props)
