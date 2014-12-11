f1b5c511b13d95218cf3f7ff4e95cb862b0952c9
Piotr Kolaczkowski <pkolaczk@datastax.com>
2014-12-11 11:37:18 +0100

    SPARKC-7: Refactor and fix predicate pushdown in SparkSQL code. Fixes #454.

--- spark-cassandra-connector/src/main/scala/org/apache/spark/sql/cassandra/CassandraStrategies.scala
+++ spark-cassandra-connector/src/main/scala/org/apache/spark/sql/cassandra/CassandraStrategies.scala
@@ -9,33 +9,12 @@ import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.columnar.InMemoryRelation
 import org.apache.spark.sql.execution.SparkPlan
 
-import scala.collection.mutable.ListBuffer
-
 private[cassandra] trait CassandraStrategies {
 
   // Possibly being too clever with types here... or not clever enough.
   self: SQLContext#SparkPlanner =>
 
   val cassandraContext: CassandraSQLContext
-
-  private[this] def pushdownAble(predicate: Expression) : Boolean = predicate match {
-    case e: EqualTo             => true
-    case l: LessThan            => true
-    case le: LessThanOrEqual    => true
-    case g: GreaterThan         => true
-    case ge: GreaterThanOrEqual => true
-    case _                      => false
-  }
-
-  private[this] def isIN(predicate: Expression) : Boolean = predicate match {
-    case l: In => true
-    case _     => false
-  }
-
-  private[this] def isEqualTo(predicate: Expression) : Boolean = predicate match {
-    case e: EqualTo => true
-    case _          => false
-  }
 
   object DataSinks extends Strategy {
     def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
@@ -49,186 +28,153 @@ private[cassandra] trait CassandraStrategies {
   }
 
   /**
-   * Retrieves data using a CassandraTableScan.  Partition pruning predicates are also detected and
-   * applied.
-   *  1. Only push down none-partition column predicates with =, >, <, >=, <= predicate
+   * Retrieves data using a CassandraTableScan.
+   * Partition pruning predicates are also detected an applied.
+   *  1. Only push down no-partition key column predicates with =, >, <, >=, <= predicate
    *  2. Only push down primary key column predicates with = or IN predicate.
-   *  3. If there are regular columns in the pushdown predicates. There should have
+   *  3. If there are regular columns in the pushdown predicates, they should have
    *     at least one EQ expression on an indexed column and no IN predicates.
-   *  4. All partition column predicates must in the predicates to be pushed down,
-   *     only the last part of the partition key can be IN predicate. For each partition column,
+   *  4. All partition column predicates must be included in the predicates to be pushed down,
+   *     only the last part of the partition key can be an IN predicate. For each partition column,
    *     only one predicate is allowed.
-   *  5. For cluster column predicates, only last predicate can be none-EQ predicate
+   *  5. For cluster column predicates, only last predicate can be non-EQ predicate
    *     including IN predicate, and preceding column predicates must be EQ predicates.
-   *     If there is only one cluster column predicates, the predicates could be any predicate.
-   *  6. There is no pushdown predicates if there is any OR condition or NOT IN condition
+   *     If there is only one cluster column predicate, the predicates could be any non-IN predicate.
+   *  6. There is no pushdown predicates if there is any OR condition or NOT IN condition.
+   *  7. We're not allowed to push down multiple predicates for the same column if any of them
+   *     is equality or IN predicate.
    */
-  //TODO: index on collection pushdown
+  // TODO: index on collection pushdown
   object CassandraTableScans extends Strategy with Logging {
 
-    def partitionColumnPushDown(partitionColumnPredicates: Seq[Expression], partitionColumns: Seq[String]):
-      (Boolean, Boolean, Seq[Expression], Seq[Expression]) = {
-        val lastPartitionColumn = partitionColumns.last
-        var partitionColumnPushdown = ListBuffer[Expression]()
-        var partitionColumnOther = ListBuffer[Expression]()
-        var namePredicateMap = Map[String, Expression]()
-        for (predicate <- partitionColumnPredicates) {
-          val name = predicate.references.map(_.name).head
-          if (!namePredicateMap.contains(name) && 
-              (isEqualTo(predicate) || name == lastPartitionColumn && isIN(predicate))) {
-            partitionColumnPushdown += predicate
-            namePredicateMap += name -> predicate
-          } else {
-            partitionColumnOther += predicate
-          }
-        }
+    private class PredicatePushDown(predicates: Seq[Expression], relation: CassandraRelation) {
 
-        val partitionColumnPd = partitionColumnPushdown.toSeq
-        val partitionColumnRemaining = partitionColumnOther.toSeq
-        if (partitionColumnPd.size == partitionColumns.size) {
-          if (partitionColumnPd.filter(isIN).nonEmpty) {
-            (true, true, partitionColumnPd, partitionColumnRemaining)
-          } else {
-            (true, false, partitionColumnPd, partitionColumnRemaining)
-          }
-        } else {
-          (false, false, partitionColumnPd, partitionColumnRemaining)
-        }
+      private def isEqualTo(predicate: Expression): Boolean =
+        predicate.isInstanceOf[EqualTo]
+
+      private def isIn(predicate: Expression) : Boolean =
+        predicate.isInstanceOf[In]
+
+      private def isRangeComparison(predicate: Expression) : Boolean = predicate match {
+        case _: LessThan           => true
+        case _: LessThanOrEqual    => true
+        case _: GreaterThan        => true
+        case _: GreaterThanOrEqual => true
+        case _                     => false
+      }
+
+      private def isSingleColumn(predicate: Expression) =
+        predicate.references.size == 1
+
+      private val partitionKeyColumns = relation.partitionColumns.map(_.name)
+      private val clusteringColumns = relation.clusterColumns.map(_.name)
+      private val indexedColumns = relation.indexedColumns.map(_.name)
+      private val regularColumns = relation.regularColumns.map(_.name)
+      private val allColumns = partitionKeyColumns ++ clusteringColumns ++ regularColumns
+
+      private val singleColumnPredicates = predicates.filter(isSingleColumn)
+
+      private val eqPredicates = singleColumnPredicates.filter(isEqualTo)
+      private val eqPredicatesByName = eqPredicates.groupBy(predicateColumnName)
+        .mapValues(_.take(1))       // take(1) in order not to push down more than one EQ predicate for the same column
+        .withDefaultValue(Seq.empty)
+
+      private val inPredicates = singleColumnPredicates.filter(isIn)
+      private val inPredicatesByName = inPredicates.groupBy(predicateColumnName)
+        .mapValues(_.take(1))      // take(1) in order not to push down more than one IN predicate for the same column
+        .withDefaultValue(Seq.empty)
+
+      private val rangePredicates = singleColumnPredicates.filter(isRangeComparison)
+      private val rangePredicatesByName = rangePredicates.groupBy(predicateColumnName).withDefaultValue(Seq.empty)
+
+      /** Returns the only column name referenced in the predicate */
+      private def predicateColumnName(predicate: Expression) = {
+        require(predicate.references.size == 1, "Given predicate is not a single column predicate: " + predicate)
+        predicate.references.head.name
+      }
+
+      /** Returns a first non-empty sequence. If not found, returns an empty sequence. */
+      private def firstNonEmptySeq[T](sequences: Seq[T]*): Seq[T] =
+        sequences.find(_.nonEmpty).getOrElse(Seq.empty[T])
+
+      /**
+       * Selects partition key predicates for pushdown:
+       * 1. Partition key predicates must be equality or IN predicates.
+       * 2. Only the last partition key column predicate can be an IN.
+       * 3. All partition key predicates must be used or none.
+       */
+      private val partitionKeyPredicatesToPushDown: Seq[Expression] = {
+        val (eqColumns, otherColumns) = partitionKeyColumns.span(eqPredicatesByName.contains)
+        val inColumns = otherColumns.headOption.toSeq.filter(inPredicatesByName.contains)
+        if (eqColumns.size + inColumns.size == partitionKeyColumns.size)
+          eqColumns.flatMap(eqPredicatesByName) ++ inColumns.flatMap(inPredicatesByName)
+        else
+          Nil
+      }
+
+      /**
+       * Selects clustering key predicates for pushdown:
+       * 1. Clustering column predicates must be equality predicates, except the last one.
+       * 2. The last predicate is allowed to be an equality or a range predicate.
+       * 3. The last predicate is allowed to be an IN predicate only if it was preceded by an equality predicate.
+       * 4. Consecutive clustering columns must be used, but, contrary to partition key, the tail can be skipped.
+       */
+      private val clusteringColumnPredicatesToPushDown: Seq[Expression] = {
+        val (eqColumns, otherColumns) = clusteringColumns.span(eqPredicatesByName.contains)
+        val eqPredicates = eqColumns.flatMap(eqPredicatesByName)
+        val optionalNonEqPredicate = for {
+          c <- otherColumns.headOption.toSeq
+          p <- firstNonEmptySeq(rangePredicatesByName(c), inPredicatesByName(c).filter(_ => eqColumns.nonEmpty))
+        } yield p
+
+        eqPredicates ++ optionalNonEqPredicate
+      }
+
+      /**
+       * Selects indexed and regular column predicates for pushdown:
+       * 1. At least one indexed column must be present in an equality predicate to be pushed down.
+       * 2. Regular column predicates can be either equality or range predicates.
+       * 3. If multiple predicates use the same column, equality predicates are preferred over range predicates.
+       */
+      private val indexedColumnPredicatesToPushDown: Seq[Expression] = {
+        val inPredicateInPrimaryKey = partitionKeyPredicatesToPushDown.exists(isIn)
+        val eqIndexedColumns = indexedColumns.filter(eqPredicatesByName.contains)
+        val eqIndexedPredicates = eqIndexedColumns.flatMap(eqPredicatesByName)
+        val nonIndexedPredicates = for {
+          c <- allColumns if !eqIndexedColumns.contains(c)
+          p <- firstNonEmptySeq(eqPredicatesByName(c), rangePredicatesByName(c))
+        } yield p
+
+        if (!inPredicateInPrimaryKey && eqIndexedColumns.nonEmpty)
+          eqIndexedPredicates ++ nonIndexedPredicates
+        else
+          Nil
+      }
+
+      val predicatesToPushDown = (
+        partitionKeyPredicatesToPushDown ++
+        clusteringColumnPredicatesToPushDown ++
+        indexedColumnPredicatesToPushDown).distinct
+
+      val predicatesToPreserve = predicates.filterNot(predicatesToPushDown.toSet.contains)
     }
 
-    def clusterColumnPushDown(clusterColumnPredicates: Seq[Expression], clusterColumns: Seq[String]):
-      (Boolean, Seq[Expression], Seq[Expression]) = {
-        var clusterColumnPushdown = ListBuffer[Expression]()
-        var clusterColumnOther = ListBuffer[Expression]()
-        var namePredicateMap = Map[String, Expression]()
-        var clusterColumnHasINClause = false
-        for (predicate <- clusterColumnPredicates) {
-          val name = predicate.references.map(_.name).head
-          if (!namePredicateMap.contains(name) && isEqualTo(predicate)) {
-            clusterColumnPushdown += predicate
-            namePredicateMap += name -> predicate
-          } else {
-              clusterColumnOther += predicate
-          }
-          if (!clusterColumnHasINClause && isIN(predicate))
-            clusterColumnHasINClause = true
-        }
-
-        var restoreFollowingPredicate = false
-        var restoreFollowingColumns = ListBuffer[String]()
-        var lastColumnPushdown = new String("")
-        for (column <- clusterColumns) {
-          if (restoreFollowingPredicate)
-            restoreFollowingColumns += column
-          if (!restoreFollowingPredicate && !namePredicateMap.contains(column)) {
-            restoreFollowingColumns += column
-            lastColumnPushdown = column
-            restoreFollowingPredicate = true
-          }
-        }
-
-        logInfo(s"Cluster column intermediate pushed down predicates: ${clusterColumnPushdown.toString}")
-
-        val (other, pd) = clusterColumnPushdown.toSeq.partition {
-          predicate => predicate.references.map(_.name).toSet.subsetOf(restoreFollowingColumns.toSet)
-        }
-
-        logInfo(s"Last cluster column pushed down: $lastColumnPushdown")
-        logInfo(s"Other cluster column predicates: ${clusterColumnOther.toString}")
-
-        val (push, left) = clusterColumnOther.toSeq.partition {
-          predicate => predicate.references.map(_.name).head == lastColumnPushdown && (isIN(predicate) || pushdownAble(predicate))
-        }
-
-        (clusterColumnHasINClause, pd ++ push, other ++ left)
-    }
-
-    // Final predicate pushdown
-    def pushdown(partitionColumnPushdownable: Boolean, partitionColumnHasINClause: Boolean,
-        partitionColumnPd: Seq[Expression], partitionColumnRemaining: Seq[Expression], partitionColumnPredicates: Seq[Expression],
-        clusterColumnHasINClause: Boolean, clusterColumnPd: Seq[Expression], clusterColumnRemaining: Seq[Expression],
-        indexedColumnPd: Seq[Expression], regularColumnPd: Seq[Expression], regularColumnOther:Seq[Expression],
-        regularColumnPredicates: Seq[Expression]) : (Seq[Expression], Seq[Expression]) = {
-
-        // No regular column predicates pushdown if there are no indexed column predicates or no EQ indexed column
-        // predicates
-        if (indexedColumnPd.isEmpty || indexedColumnPd.filter(isEqualTo).isEmpty) {
-          // Only partition column predicates and cluster column predicates are pushed down
-          if (partitionColumnPushdownable) {
-            logInfo("Partition column predicates are pushed down.")
-            (clusterColumnPd ++ partitionColumnPd, regularColumnPredicates ++ clusterColumnRemaining ++ partitionColumnRemaining)
-          } else {
-            (clusterColumnPd, regularColumnPredicates  ++ clusterColumnRemaining ++ partitionColumnPredicates)
-          }
-        } else {
-          // No IN predicate in primary key column predicates
-          val (pushdown, remain) =
-            if (partitionColumnPushdownable && !partitionColumnHasINClause)
-              (regularColumnPd ++ partitionColumnPd, regularColumnOther ++ partitionColumnRemaining)
-            else
-              (regularColumnPd, regularColumnOther ++ partitionColumnPredicates)
-
-          if (!clusterColumnHasINClause) {
-            (pushdown ++ clusterColumnPd, remain ++ clusterColumnRemaining)
-          } else {
-            val (other, pd) = clusterColumnPd.partition(isIN)
-            (pushdown ++ pd, remain ++ clusterColumnRemaining ++ other)
-          }
-        }
-    }
 
     def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
       case PhysicalOperation(projectList, predicates, relation: CassandraRelation) =>
-
         logInfo(s"projectList: ${projectList.toString()}")
         logInfo(s"predicates: ${predicates.toString()}")
+        val pushDown = new PredicatePushDown(predicates, relation)
+        val pushdownPredicates = pushDown.predicatesToPushDown
+        val otherPredicates = pushDown.predicatesToPreserve
+        logInfo(s"pushdown predicates:  ${pushdownPredicates.toString()}")
+        logInfo(s"remaining predicates: ${otherPredicates.toString()}")
 
-        val indexedColumns = relation.indexedColumns.map(_.name)
-        val regularColumns = relation.regularColumns.map(_.name)
-        val partitionColumns = relation.partitionColumns.map(_.name)
-        val clusterColumns = relation.clusterColumns.map(_.name)
-        val allColumns = partitionColumns ++ clusterColumns ++ regularColumns
-
-        val allColumnPredicates = predicates.filter(_.references.map(_.name).toSet.subsetOf(allColumns.toSet))
-        if (allColumnPredicates.isEmpty) {
-          pruneFilterProject(
-            projectList,
-            predicates,
-            identity[Seq[Expression]],
-            CassandraTableScan(_, relation, Seq.empty[Expression])(cassandraContext)) :: Nil
-        } else {
-          // Partition column predicates pushdown
-          val partitionColumnPredicates = predicates.filter(_.references.map(_.name).toSet.subsetOf(partitionColumns.toSet))
-          val (partitionColumnPushdownable, partitionColumnHasINClause, partitionColumnPd, partitionColumnRemaining)
-            = partitionColumnPushDown(partitionColumnPredicates, partitionColumns)
-
-          // Cluster column predicates pushdown
-          val clusterColumnPredicates = predicates.filter(_.references.map(_.name).toSet.subsetOf(clusterColumns.toSet))
-          val (clusterColumnHasINClause, clusterColumnPd, clusterColumnRemaining) = clusterColumnPushDown(clusterColumnPredicates, clusterColumns)
-
-          logInfo(s"Cluster column final pushdown predicates: ${clusterColumnPd.toString}")
-          logInfo(s"Cluster column remaining predicates: ${clusterColumnRemaining.toString}")
-
-          val regularColumnPredicates = predicates.filter(_.references.map(_.name).toSet.subsetOf(regularColumns.toSet))
-          val (regularColumnPd, regularColumnOther) = regularColumnPredicates.partition { pushdownAble }
-          val indexedColumnPd = regularColumnPd.filter(_.references.map(_.name).toSet.subsetOf(indexedColumns.toSet))
-
-          logInfo(s"Indexed column pushdownable predicates: ${indexedColumnPd.toString()}")
-
-          val (pushdownPredicates, otherPredicates) = pushdown(partitionColumnPushdownable, partitionColumnHasINClause,
-            partitionColumnPd, partitionColumnRemaining, partitionColumnPredicates,
-            clusterColumnHasINClause, clusterColumnPd, clusterColumnRemaining,
-            indexedColumnPd, regularColumnPd, regularColumnOther, regularColumnPredicates)
-
-          logInfo(s"pushdown predicates:  ${pushdownPredicates.toString()}")
-          logInfo(s"remaining predicates: ${otherPredicates.toString()}")
-
-          pruneFilterProject(
-            projectList,
-            otherPredicates,
-            identity[Seq[Expression]],
-            CassandraTableScan(_, relation, pushdownPredicates)(cassandraContext)) :: Nil
-        }
+        pruneFilterProject(
+          projectList,
+          otherPredicates,
+          identity[Seq[Expression]],
+          CassandraTableScan(_, relation, pushdownPredicates)(cassandraContext)) :: Nil
       case _ =>
         Nil
     }
