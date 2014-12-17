46553cda7667510ec0a6922a14c95c31e532121d
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2014-12-17 14:44:24 +0100

    wip-442: Created BatchLevel enumeration to distinguish different ways of grouping batches; added to WriteConf and updated API

---
+++ spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/writer/BatchLevel.scala
@@ -0,0 +1,24 @@
+package com.datastax.spark.connector.writer
+
+sealed trait BatchLevel
+
+object BatchLevel {
+
+  /** Any row can be added to any batch. This works the same as previous batching implementation. */
+  case object All extends BatchLevel
+
+  /** Each batch is associated with a set of replicas. If a set of replicas for the inserted row is
+    * the same as it is for a batch, the row can be added to the batch. */
+  case object ReplicaSet extends BatchLevel
+
+  /** Each batch is associated with a partition key. If the partition key of the inserted row is the
+    * same as it is for a batch, the row can be added to the batch. */
+  case object Partition extends BatchLevel
+
+  def apply(name: String): BatchLevel = name.toLowerCase match {
+    case "all" => All
+    case "replicaset" => ReplicaSet
+    case "partition" => Partition
+    case _ => throw new IllegalArgumentException(s"Invalid batch level: $name")
+  }
+}
