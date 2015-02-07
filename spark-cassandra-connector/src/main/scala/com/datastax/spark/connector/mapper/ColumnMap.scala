0295a4cbf484c00a71ebf4c434946c81743136d6
Piotr Kolaczkowski <pkolaczk@datastax.com>
2015-02-07 13:49:59 +0100

    SPARKC-38: Added ScalaDoc to ColumnMap.

--- spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/mapper/ColumnMap.scala
+++ spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/mapper/ColumnMap.scala
@@ -2,14 +2,22 @@ package com.datastax.spark.connector.mapper
 
 import com.datastax.spark.connector.ColumnRef
 
-/** Associates constructor parameters and property accessors with table columns */
+/** Associates constructor parameters and property accessors with Cassandra table columns. */
 trait ColumnMap extends Serializable {
+
+  /** A sequence of column references associated with parameters of the main constructor */
   def constructor: Seq[ColumnRef]
 
+  /** Maps a getter method name to a column reference */
   def getters: Map[String, ColumnRef]
 
+  /** Maps a setter method name to a column reference */
   def setters: Map[String, ColumnRef]
 
+  /** Whether Java nulls are allowed to be directly set as object properties.
+    * This is desired for compatibility with Java classes, but in Scala, we have Option,
+    * therefore we need to fail fast if one wants to assign a Cassandra null
+    * value to a non-optional property. */
   def allowsNull: Boolean
 }
 
@@ -17,4 +25,3 @@ case class SimpleColumnMap(constructor: Seq[ColumnRef],
                            getters: Map[String, ColumnRef],
                            setters: Map[String, ColumnRef],
                            allowsNull: Boolean = false) extends ColumnMap
-
