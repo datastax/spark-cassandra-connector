05c5dc8c0ac1118f0925ea2e1aec6785cba0e850
Piotr Kolaczkowski <pkolaczk@datastax.com>
2015-03-19 16:12:58 +0100

    Set version to 1.3.0-SNAPSHOT

--- project/Settings.scala
+++ project/Settings.scala
@@ -36,7 +36,7 @@ object Settings extends Build {
 
   lazy val buildSettings = Seq(
     organization         := "com.datastax.spark",
-    version in ThisBuild := "1.2.0-SNAPSHOT",
+    version in ThisBuild := "1.3.0-SNAPSHOT",
     scalaVersion         := Versions.scalaVersion,
     crossScalaVersions   := Versions.crossScala,
     crossVersion         := CrossVersion.binary,
