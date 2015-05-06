e6a9de3ac039c97299eabcbd254b3446885116aa
Alex Liu <alex_liu68@yahoo.com>
2015-05-06 14:07:01 -0700

    SPARKC-112 use host address to compare InetAddress

--- spark-cassandra-connector/src/main/scala/org/apache/spark/sql/types/InetAddressType.scala
+++ spark-cassandra-connector/src/main/scala/org/apache/spark/sql/types/InetAddressType.scala
@@ -2,6 +2,8 @@ package org.apache.spark.sql.types
 
 import java.net.InetAddress
 
+import org.apache.spark.Logging
+
 import scala.reflect.runtime.universe.typeTag
 
 import org.apache.spark.annotation.DeveloperApi
@@ -15,17 +17,20 @@ import org.apache.spark.sql.catalyst.ScalaReflectionLock
  * @group dataType
  */
 @DeveloperApi
-class InetAddressType private() extends NativeType with PrimitiveType {
+class InetAddressType private() extends NativeType with PrimitiveType with Logging {
   // The companion object and this class is separated so the companion object also subclasses
   // this type. Otherwise, the companion object would be of type "InetAddressType$" in byte code.
   // Defined with a private constructor so the companion object is the only possible instantiation.
   private[sql] type JvmType = InetAddress
   @transient private[sql] lazy val tag = ScalaReflectionLock.synchronized { typeTag[JvmType] }
 
-  // Convert to string to compare InetAddress because it doesn't support comparison.
+  // Convert to host address to compare InetAddress because it doesn't support comparison.
   // It's not a good solution though.
   private[sql] val ordering = new Ordering[JvmType] {
-    def compare(x: InetAddress, y: InetAddress) = x.toString.compareTo(y.toString)
+    def compare(x: InetAddress, y: InetAddress) = {
+      logWarning("InetAddress doesn't support comparison. Convert it to host address to compare.")
+      x.getHostAddress.compareTo(y.getHostAddress)
+    }
   }
   /**
    * The default size of a value of the StringType is 4096 bytes.
