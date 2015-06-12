94caae13eaf7f24de60f393fcce0d4d0a57a63bd
Piotr Kolaczkowski <pkolaczk@datastax.com>
2015-06-12 21:13:18 +0200

    Set version to 1.4.0-M1, update CHANGES.txt.

--- project/Settings.scala
+++ project/Settings.scala
@@ -49,7 +49,7 @@ object Settings extends Build {
 
   lazy val buildSettings = Seq(
     organization         := "com.datastax.spark",
-    version in ThisBuild := s"1.3.0-M2$versionSuffix",
+    version in ThisBuild := s"1.4.0-M1$versionSuffix",
     scalaVersion         := Versions.scalaVersion,
     crossScalaVersions   := Versions.crossScala,
     crossVersion         := CrossVersion.binary,
