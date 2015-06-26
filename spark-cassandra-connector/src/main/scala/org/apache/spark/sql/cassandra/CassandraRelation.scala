6ef5be8c62839ff29fbf735f7e33be724f880482
Piotr Kolaczkowski <pkolaczk@datastax.com>
2015-06-26 18:05:34 +0200

    Revert "Merge pull request #734 from datastax/SPARKC-129"
    This reverts commit cd5fe54b45637f57c84b639b6e90b1bf275314fa, reversing
    changes made to 7212c464c73e1a8c661b15f3143301fe6eb9dd16.
    
    Conflicts:
    	CHANGES.txt

--- spark-cassandra-connector/src/main/scala/org/apache/spark/sql/cassandra/CassandraRelation.scala
+++ spark-cassandra-connector/src/main/scala/org/apache/spark/sql/cassandra/CassandraRelation.scala
@@ -1,10 +1,13 @@
 package org.apache.spark.sql.cassandra
 
+import com.datastax.spark.connector
 import com.datastax.spark.connector.cql.{ColumnDef, TableDef}
 import com.datastax.spark.connector.types.UDTFieldDef
 
 import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
 import org.apache.spark.sql.catalyst.plans.logical.{Statistics, LeafNode}
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.{catalyst, types}
 
 private[cassandra] case class CassandraRelation
     (tableDef: TableDef, alias: Option[String], cluster: Option[String] = None)
@@ -22,7 +25,7 @@ private[cassandra] case class CassandraRelation
 
   def columnToAttribute(column: ColumnDef): AttributeReference = {
     // Since data can be dumped in randomly with no validation, everything is nullable.
-    val catalystType = DataTypeConverter.catalystDataType(column.columnType, nullable = true)
+    val catalystType = ColumnDataType.catalystDataType(column.columnType, nullable = true)
     val qualifiers = tableDef.tableName +: alias.toSeq
     new AttributeReference(column.columnName, catalystType, nullable = true)(qualifiers = qualifiers)
   }
@@ -36,4 +39,44 @@ private[cassandra] case class CassandraRelation
   )
 
   def tableName = tableDef.tableName
+}
+
+object ColumnDataType {
+
+  private val primitiveTypeMap = Map[connector.types.ColumnType[_], types.DataType](
+    connector.types.TextType       -> types.StringType,
+    connector.types.AsciiType      -> types.StringType,
+    connector.types.VarCharType    -> types.StringType,
+
+    connector.types.BooleanType    -> types.BooleanType,
+
+    connector.types.IntType        -> types.IntegerType,
+    connector.types.BigIntType     -> types.LongType,
+    connector.types.CounterType    -> types.LongType,
+    connector.types.FloatType      -> types.FloatType,
+    connector.types.DoubleType     -> types.DoubleType,
+  
+    connector.types.VarIntType     -> types.DecimalType(), // no native arbitrary-size integer type
+    connector.types.DecimalType    -> types.DecimalType(),
+
+    connector.types.TimestampType  -> types.TimestampType,
+    connector.types.InetType       -> types.StringType,
+    connector.types.UUIDType       -> types.StringType,
+    connector.types.TimeUUIDType   -> types.StringType,
+    connector.types.BlobType       -> types.ByteType
+  )
+
+  def catalystDataType(cassandraType: connector.types.ColumnType[_], nullable: Boolean): types.DataType = {
+
+    def catalystStructField(field: UDTFieldDef): StructField =
+      StructField(field.columnName, catalystDataType(field.columnType, nullable = true), nullable = true)
+
+    cassandraType match {
+      case connector.types.SetType(et)                => types.ArrayType(primitiveTypeMap(et), nullable)
+      case connector.types.ListType(et)               => types.ArrayType(primitiveTypeMap(et), nullable)
+      case connector.types.MapType(kt, vt)            => types.MapType(primitiveTypeMap(kt), primitiveTypeMap(vt), nullable)
+      case connector.types.UserDefinedType(_, fields) => types.StructType(fields.map(catalystStructField))
+      case _                                          => primitiveTypeMap(cassandraType)
+    }
+  }
 }
