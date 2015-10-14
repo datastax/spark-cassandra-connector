58f6c226adb5224050428358226acf3f1a47bae9
Russell Spitzer <Russell.Spitzer@gmail.com>
2015-10-14 14:39:24 -0700

    SPARKC-249: Fix merge strategy for netty.io.properties

--- project/Settings.scala
+++ project/Settings.scala
@@ -235,6 +235,7 @@ object Settings extends Build {
     assemblyMergeStrategy in assembly <<= (assemblyMergeStrategy in assembly) {
       (old) => {
         case PathList("com", "google", xs @ _*) => MergeStrategy.last
+        case PathList("META-INF", "io.netty.versions.properties") => MergeStrategy.last
         case x => old(x)
       }
     }
