b406bc107f398d6c595a6494d256203d1a0265a0
Russell Spitzer <Russell.Spitzer@gmail.com>
2015-12-29 10:51:10 -0800

    SPARKC-306: Remove Special Matching for Dataframe Writers
    The type converters are fully capabible of handeling all of the
    edge cases for DataFrame -> CassandraType conversions. We can use this
    instead of the custom code previously in the SqlRowWriter class. In
    addtiion, added test cases for the special matching previously done.

--- spark-cassandra-connector/src/it/scala/com/datastax/spark/connector/rdd/typeTests/InetTypeTest.scala
+++ spark-cassandra-connector/src/it/scala/com/datastax/spark/connector/rdd/typeTests/InetTypeTest.scala
@@ -1,8 +1,11 @@
 package com.datastax.spark.connector.rdd.typeTests
 
 import java.net.InetAddress
+import org.apache.spark.sql.SaveMode
+
 import scala.collection.JavaConverters._
 
+case class InetStringRow(pkey: String, ckey1: String, ckey2: String, data1: String)
 class InetTypeTest extends AbstractTypeTest[InetAddress, InetAddress] {
   override val typeName = "inet"
 
@@ -11,6 +14,23 @@ class InetTypeTest extends AbstractTypeTest[InetAddress, InetAddress] {
 
   override def getDriverColumn(row: com.datastax.driver.core.Row, colName: String): InetAddress = {
     row.getInet(colName)
+  }
+
+  "A String DataFrame" should "write to C* Inets" in {
+    val InetString = "111.111.111.111"
+    val stringRDD = sc.parallelize(Seq(InetStringRow(InetString, InetString, InetString, InetString)))
+    val stringDf = sqlContext.createDataFrame(stringRDD)
+
+    val normOptions = Map("keyspace" -> keyspaceName, "table" -> typeNormalTable)
+    stringDf.write
+      .format("org.apache.spark.sql.cassandra")
+      .options(normOptions)
+      .mode(SaveMode.Append)
+      .save()
+
+    val row = conn.withSessionDo(session =>
+      session.execute(s"SELECT * FROM $keyspaceName.$typeNormalTable WHERE pkey = '$InetString' and ckey1 = '$InetString'").one)
+    row.getInet("data1") should be (InetAddress.getByName(InetString))
   }
 }
 
