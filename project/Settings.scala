526e7b68f7a44da93e021b86a7658fa61c9cca10
Piotr Kolaczkowski <pkolaczk@datastax.com>
2016-01-07 10:09:56 +0100

    Prepare release 1.5.0-RC1

--- project/Settings.scala
+++ project/Settings.scala
@@ -85,7 +85,7 @@ object Settings extends Build {
 
   lazy val buildSettings = Seq(
     organization         := "com.datastax.spark",
-    version in ThisBuild := s"1.5.0-M4$versionSuffix",
+    version in ThisBuild := s"1.5.0-RC1$versionSuffix",
     scalaVersion         := Versions.scalaVersion,
     crossScalaVersions   := Versions.crossScala,
     crossVersion         := CrossVersion.binary,
