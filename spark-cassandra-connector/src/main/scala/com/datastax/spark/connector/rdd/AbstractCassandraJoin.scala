3b3a8fbf74329ca533c4daccd50184553949289d
Eric Meisel <eric.meisel@influencehealth.com>
2016-12-15 22:46:25 -0600

    Spark-433 backport

--- spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/rdd/AbstractCassandraJoin.scala
+++ spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/rdd/AbstractCassandraJoin.scala
@@ -111,8 +111,8 @@ private[rdd] trait AbstractCassandraJoin[L, R] {
     require(
       joinColumnPredicates.isEmpty,
       s"""Columns specified in both the join on clause and the where clause.
-          |Partition key columns are always part of the join clause.
-          |Columns in both: ${joinColumnPredicates.mkString(", ")}""".stripMargin
+         |Partition key columns are always part of the join clause.
+         |Columns in both: ${joinColumnPredicates.mkString(", ")}""".stripMargin
     )
 
     logDebug("Generating Single Key Query Prepared Statement String")
@@ -127,7 +127,7 @@ private[rdd] trait AbstractCassandraJoin[L, R] {
     val query =
       s"SELECT $columns " +
         s"FROM $quotedKeyspaceName.$quotedTableName " +
-        s"WHERE $filter $limitClause $orderBy"
+        s"WHERE $filter $orderBy $limitClause"
     logDebug(s"Query : $query")
     query
   }
@@ -148,7 +148,7 @@ private[rdd] trait AbstractCassandraJoin[L, R] {
     val bsb = boundStatementBuilder(session)
     val metricsUpdater = InputMetricsUpdater(context, readConf)
     val rowIterator = fetchIterator(session, bsb, left.iterator(split, context))
-    val countingIterator = new CountingIterator(rowIterator, limit)
+    val countingIterator = new CountingIterator(rowIterator, None)
 
     context.addTaskCompletionListener { (context) =>
       val duration = metricsUpdater.finish() / 1000000000d
