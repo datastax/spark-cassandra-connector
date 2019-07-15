94342842df643b3ce1eed056a2e84779c4e55696
Russell Spitzer <Russell.Spitzer@gmail.com>
2019-07-15 17:14:29 -0500

    DSP-18857: Multiple Retry Policy Update

--- connector/src/main/scala/com/datastax/spark/connector/cql/MultipleRetryPolicy.scala
+++ connector/src/main/scala/com/datastax/spark/connector/cql/MultipleRetryPolicy.scala
@@ -1,9 +1,10 @@
 package com.datastax.spark.connector.cql
 
-import com.datastax.driver.core.exceptions._
-import com.datastax.driver.core.policies.RetryPolicy
-import com.datastax.driver.core.policies.RetryPolicy.RetryDecision
-import com.datastax.driver.core.{ConsistencyLevel, Statement, WriteType}
+import com.datastax.oss.driver.api.core.ConsistencyLevel
+import com.datastax.oss.driver.api.core.cql.Statement
+import com.datastax.oss.driver.api.core.retry.{RetryDecision, RetryPolicy}
+import com.datastax.oss.driver.api.core.servererrors.{CoordinatorException, WriteType}
+import com.datastax.oss.driver.api.core.session.Request
 
 /** Always retries with the same CL (null forces the original statement CL see SPARKC-494),
   *  constant number of times, regardless of circumstances
@@ -13,68 +14,48 @@ import com.datastax.driver.core.{ConsistencyLevel, Statement, WriteType}
 class MultipleRetryPolicy(maxRetryCount: Int)
   extends RetryPolicy{
 
-  // scalastyle:off null
   private def retryManyTimesOrThrow(nbRetry: Int): RetryDecision = maxRetryCount match {
-    case -1 => RetryDecision.retry(null)
+    case -1 => RetryDecision.RETRY_SAME
     case maxRetries =>
       if (nbRetry < maxRetries) {
-        RetryDecision.retry(null)
+        RetryDecision.RETRY_SAME
       } else {
-        RetryDecision.rethrow()
+        RetryDecision.RETHROW
       }
   }
 
-  private def retryOnceOrThrow(nbRetry: Int): RetryDecision = {
-    if (nbRetry == 0) {
-      RetryDecision.retry(null)
-    } else {
-      RetryDecision.rethrow()
-    }
-  }
-
-  override def init(cluster: com.datastax.driver.core.Cluster): Unit = {}
-  override def close(): Unit = { }
-
-
   override def onReadTimeout(
-      stmt: Statement,
-      cl: ConsistencyLevel,
-      requiredResponses: Int,
-      receivedResponses: Int,
-      dataRetrieved: Boolean,
-      nbRetry: Int): RetryDecision = {
-    retryManyTimesOrThrow(nbRetry)
-  }
-
-  /**
-    * We need to handle these exceptions at a level where we
-    * can wait to retry.
-    */
-  override def onRequestError(
-     stmt: Statement,
-     cl: ConsistencyLevel,
-     ex: DriverException,
-     nbRetry: Int): RetryDecision = {
-      RetryDecision.rethrow()
-  }
+    request: Request,
+    cl: ConsistencyLevel,
+    blockFor: Int,
+    received: Int,
+    dataPresent: Boolean,
+    retryCount: Int): RetryDecision = retryManyTimesOrThrow(retryCount)
 
   override def onWriteTimeout(
-     stmt: Statement,
-     cl: ConsistencyLevel,
-     writeType: WriteType,
-     requiredAcks: Int,
-     receivedAcks: Int,
-     nbRetry: Int): RetryDecision = {
-    retryManyTimesOrThrow(nbRetry)
-  }
+    request: Request,
+    cl: ConsistencyLevel,
+    writeType: WriteType,
+    blockFor: Int,
+    received: Int,
+    retryCount: Int): RetryDecision = retryManyTimesOrThrow(retryCount)
 
   override def onUnavailable(
-      stmt: Statement,
-      cl: ConsistencyLevel,
-      requiredReplica: Int,
-      aliveReplica: Int,
-      nbRetry: Int): RetryDecision = {
-    retryManyTimesOrThrow(nbRetry)
-  }
+    request: Request,
+    cl: ConsistencyLevel,
+    required: Int,
+    alive: Int,
+    retryCount: Int): RetryDecision = retryManyTimesOrThrow(retryCount)
 
+  override def onRequestAborted(
+    request: Request,
+    error: Throwable,
+    retryCount: Int): RetryDecision = retryManyTimesOrThrow(retryCount)
+
+  override def onErrorResponse(
+    request: Request,
+    error: CoordinatorException,
+    retryCount: Int): RetryDecision = RetryDecision.RETHROW
+
+  override def close(): Unit = {}
 }
