72ba492361972120e2a83fca2cb329a3aaa613a2
Russell Spitzer <Russell.Spitzer@gmail.com>
2019-08-05 13:04:37 -0500

    DSP-19505: Publishing the connector
    Enables publish task, will publish to destination specified in system
    properties and falls back to datastax-releases-local. Looks for a
    credential file and uses .sbt/credentials and ./sbt/credentials/deploy.
    
    Local repositories can be populated with publishM2, publishLocal

---
+++ project/Publishing.scala
@@ -0,0 +1,39 @@
+import java.io.File
+
+import sbt.Path
+import sbt._
+import sbt.librarymanagement.Resolver
+import sbt.librarymanagement.ivy.Credentials
+
+import scala.sys.process._
+
+object Publishing extends sbt.librarymanagement.DependencyBuilders {
+
+  val DseRelease = "datastax-releases-local" at "https://repo.sjc.dsinternal.org/artifactory/datastax-releases-local"
+
+  val Version: String = {
+    sys.props.get("publish.version").getOrElse("git describe --tags" !!).stripLineEnd
+  }
+
+  val Repository: Option[Resolver] = {
+    Some((sys.props.get("publish.repository.name"), sys.props.get("publish.repository.location")) match {
+      case (Some(name), Some(location)) => name at location
+      case _ =>  DseRelease
+    })
+  }
+
+  val Credentials: Seq[Credentials] = {
+    val credFile = sys.props.get("publish.repository.credentials.file")
+      .map(path => sbt.librarymanagement.ivy.Credentials(new File(path)))
+
+
+    val personalCred = sbt.librarymanagement.ivy.Credentials(Path.userHome / ".sbt" / "credentials")
+    val deployCred = sbt.librarymanagement.ivy.Credentials(Path.userHome / ".sbt" / "credentials.deploy")
+
+    println(s"Reading credentials from $credFile")
+
+    Seq(credFile, Some(personalCred), Some(deployCred)).flatten
+  }
+
+
+}
