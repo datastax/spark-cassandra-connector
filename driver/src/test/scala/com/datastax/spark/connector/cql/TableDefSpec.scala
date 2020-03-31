a3abb1e396353db2a243cba155229ebe5c75f584
Russell Spitzer <russell@datastax.com>
2020-03-31 14:56:43 -0500

    Merge pull request #1239 from alexott/SPARKC-524
    [SPARKC-524] Generate a CQL Create Table from DataFrame with options
    
    (cherry picked from commit 59dfc0c7af7f906117a9df9501f442d3c6a7d49c)

--- driver/src/test/scala/com/datastax/spark/connector/cql/TableDefSpec.scala
+++ driver/src/test/scala/com/datastax/spark/connector/cql/TableDefSpec.scala
@@ -3,6 +3,8 @@ package com.datastax.spark.connector.cql
 import com.datastax.spark.connector.types._
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.collection.SortedMap
+
 class TableDefSpec extends WordSpec with Matchers {
 
   "A TableDef#cql method" should {
@@ -64,6 +66,55 @@ class TableDefSpec extends WordSpec with Matchers {
             |  "c2" map<bigint, varchar>,
             |  PRIMARY KEY (("c1"))
             |)""".stripMargin
+        )
+      }
+
+      "it contains compound partition key, if not exists, multiple clustering columns with sorting and options" in {
+        val column1 = ColumnDef("c1", PartitionKeyColumn, IntType)
+        val column2 = ColumnDef("c2", PartitionKeyColumn, VarCharType)
+        val column3 = ColumnDef("c3", ClusteringColumn(0), VarCharType)
+        val column4 = ColumnDef("c4", ClusteringColumn(1, ClusteringColumn.Descending), VarCharType)
+        val column5 = ColumnDef("c5", RegularColumn, VarCharType)
+        //
+        val tableDefSortOnly = TableDef("keyspace", "table", Seq(column1, column2), Seq(column3, column4),
+          Seq(column5), ifNotExists = true)
+        tableDefSortOnly.cql should be(
+          """CREATE TABLE IF NOT EXISTS "keyspace"."table" (
+            |  "c1" int,
+            |  "c2" varchar,
+            |  "c3" varchar,
+            |  "c4" varchar,
+            |  "c5" varchar,
+            |  PRIMARY KEY (("c1", "c2"), "c3", "c4")
+            |) WITH CLUSTERING ORDER BY ("c3" ASC, "c4" DESC)""".stripMargin
+        )
+        //
+        val tableDefSortAndOptions = TableDef("keyspace", "table", Seq(column1, column2),
+          Seq(column3, column4), Seq(column5), ifNotExists = true,
+          tableOptions = Map("dclocal_read_repair_chance"-> "0.1" ))
+        tableDefSortAndOptions.cql should be(
+          """CREATE TABLE IF NOT EXISTS "keyspace"."table" (
+            |  "c1" int,
+            |  "c2" varchar,
+            |  "c3" varchar,
+            |  "c4" varchar,
+            |  "c5" varchar,
+            |  PRIMARY KEY (("c1", "c2"), "c3", "c4")
+            |) WITH CLUSTERING ORDER BY ("c3" ASC, "c4" DESC)
+            |  AND dclocal_read_repair_chance = 0.1""".stripMargin
+        )
+        //
+        val tableDefOptionsOnly = TableDef("keyspace", "table", Seq(column1, column2),
+          Seq(column3), Seq(column5), ifNotExists = true,
+          tableOptions = Map("dclocal_read_repair_chance"-> "0.1"))
+        tableDefOptionsOnly.cql should be(
+          """CREATE TABLE IF NOT EXISTS "keyspace"."table" (
+            |  "c1" int,
+            |  "c2" varchar,
+            |  "c3" varchar,
+            |  "c5" varchar,
+            |  PRIMARY KEY (("c1", "c2"), "c3")
+            |) WITH dclocal_read_repair_chance = 0.1""".stripMargin
         )
       }
     }
