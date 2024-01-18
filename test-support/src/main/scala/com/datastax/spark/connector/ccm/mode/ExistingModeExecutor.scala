f9b3c9637a4d5a0c2bf8ee7b5c0cb5cc1151a821
Norbert Schultz <norbert.schultz@reactivecore.de>
2024-01-18 13:27:53 +0100

    Add comment

--- test-support/src/main/scala/com/datastax/spark/connector/ccm/mode/ExistingModeExecutor.scala
+++ test-support/src/main/scala/com/datastax/spark/connector/ccm/mode/ExistingModeExecutor.scala
@@ -3,6 +3,10 @@ import com.datastax.spark.connector.ccm.CcmConfig
 
 import java.nio.file.{Files, Path}
 
+/**
+ * A special ClusterModeExecutor which bypasses ccm and assumes a Cassandra instance on localhost
+ * with default ports and no authentication.
+ * */
 private[ccm] class ExistingModeExecutor(val config: CcmConfig) extends ClusterModeExecutor {
   override protected val dir: Path = Files.createTempDirectory("test")
 
